3945f0b791699b05a5102f8b41dae68bfddae2a6
JavierGar <JavierGarCan94@gmail.com>
2018-01-10 18:50:44 +0100

    testststststst comentados

--- src/minesweeper/ShowWinnersTest.java
+++ src/minesweeper/ShowWinnersTest.java
@@ -5,7 +5,10 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class ShowWinnersTest {
-
+	
+	// Test amb el que comprobem que la funcio getWinners escriu els valors correctes
+	// Com que l'acces a la base de dades no esta implementar, getWinners obté els valors
+	// de un mock object de l'objecte que tindria accés a aquesta DB
 	@Test
 	public void testShowWinners1() {
 		
